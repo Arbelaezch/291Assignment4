7c7c37881c0211c8b47417ea7c5040a67e0cd4fb
Allan Manuba <allanmanuba@gmail.com>
2019-11-22 15:03:35 -0700

    Allow duplicates in phase 2

--- phase2.sh
+++ phase2.sh
@@ -6,7 +6,7 @@ sort -u terms.txt > s_terms.txt
 sort -u recs.txt > s_recs.txt
 
 # from: docs.oracle.com/cd/E17276_01/html/api_reference/C/db_load.html
-awk -F: '{print $1; print $0}' < s_recs.txt | sed 's/\\/\\\\/g' | db_load -T -t hash re.idx
-awk -F: '{print $1; print $0}' < s_terms.txt | sed 's/\\/\\\\/g' | db_load -T -t btree te.idx
-awk -F: '{print $1; print $0}' < s_emails.txt | sed 's/\\/\\\\/g' | db_load -T -t btree em.idx
-awk -F: '{print $1; print $0}' < s_dates.txt | sed 's/\\/\\\\/g' | db_load -T -t btree da.idx
+awk -F: '{print $1; print $0}' < s_recs.txt | sed 's/\\/\\\\/g' | db_load -c duplicates=1 -T -t hash re.idx
+awk -F: '{print $1; print $0}' < s_terms.txt | sed 's/\\/\\\\/g' | db_load -c duplicates=1 -T -t btree te.idx
+awk -F: '{print $1; print $0}' < s_emails.txt | sed 's/\\/\\\\/g' | db_load -c duplicates=1 -T -t btree em.idx
+awk -F: '{print $1; print $0}' < s_dates.txt | sed 's/\\/\\\\/g' | db_load -c duplicates=1 -T -t btree da.idx
