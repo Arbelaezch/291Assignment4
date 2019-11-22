7a560504ea2fa08ed779fdb9996c88157764264c
Allan Manuba <allanmanuba@gmail.com>
2019-11-22 16:04:58 -0700

    Fix phase3?lol.py

--- phase3?lol.py
+++ phase3?lol.py
@@ -4,10 +4,10 @@ from bsddb3 import db
 
 # Name of database files to be opened
 # DB_File = "data.db"
-da = "da.idx" # Dates
-em = "em.idx" # Emails
-te = "te.idx" # Terms
-re = "re.idx" # Row IDs
+da = "da.idx"  # Dates
+em = "em.idx"  # Emails
+te = "te.idx"  # Terms
+re = "re.idx"  # Row IDs
 
 # Creates/opens primary database NOT SURE WE ACTUALLY NEED THIS.
 # P_db = db.DB() # Creates instance of Berkeley DB: database
@@ -17,19 +17,19 @@ re = "re.idx" # Row IDs
 # Creates/opens secondary databases.
 da_db = db.DB()
 da_db.set_flags(db.DB_DUP)
-da_db.open(da ,None, db.DB_BTREE, db.DB_CREATE)
+da_db.open(da, None, db.DB_BTREE, db.DB_CREATE)
 
 em_db = db.DB()
 em_db.set_flags(db.DB_DUP)
-em_db.open(em ,None, db.DB_BTREE, db.DB_CREATE)
+em_db.open(em, None, db.DB_BTREE, db.DB_CREATE)
 
 te_db = db.DB()
 te_db.set_flags(db.DB_DUP)
-te_db.open(te ,None, db.DB_BTREE, db.DB_CREATE)
+te_db.open(te, None, db.DB_BTREE, db.DB_CREATE)
 
 re_db = db.DB()
 re_db.set_flags(db.DB_DUP)
-re_db.open(re ,None, db.DB_HASH, db.DB_CREATE)
+re_db.open(re, None, db.DB_HASH, db.DB_CREATE)
 
 # Skeletons for {database} being opened for each index type.
 # database.open(DB_File ,None, db.DB_HASH, db.DB_CREATE)
@@ -38,27 +38,23 @@ re_db.open(re ,None, db.DB_HASH, db.DB_CREATE)
 # database.open(DB_File ,None, db.DB_RECNO, db.DB_CREATE)
 # The arguments correspond to (fileName, database name within the file for multiple databases, database type, flag to create database)
 
+database = db.DB()
+database.set_flags(db.DB_DUP)
+database.open(re, None, db.DB_HASH, db.DB_CREATE)
 curs = database.cursor()
 
 
 # Output: Row Id | Subject field of all matching rows.
 
 while(True):
-	query = input("Query or q to exit: ")
-	if (query == "q" or query == "Q"):
-		break
+    query = input("Query or q to exit: ")
+    if (query == "q" or query == "Q"):
+        break
 
-
-	rec = cursor.first()
-	while rec:
-		print rec
-		rec = cursor.next()
-
-
-
-
-
-
+    rec = curs.first()
+    while rec:
+        print(rec)
+        rec = curs.next()
 
 curs.close()
-databse.close()
+database.close()
